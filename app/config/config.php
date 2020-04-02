a4ddf63b58eabb7754992092fba64775258d30a7
archeonV9 <janiukbartosz@gmail.com>
2020-04-02 20:12:10 +0200

    Initial Proj

---
+++ app/config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'password');
+  define('DB_NAME', 'archeonMVC');
+
+  
+  // APP ROOT
+  define ('APPROOT', dirname(dirname(__FILE__)));  
+
+
+
+  // URL ROOT
+
+  define('URLROOT','http://localhost/yourUrl');
+
+
+
+  // SITE NAME
+
+  define('SITENAME','ArcheonMVC');
+?>
